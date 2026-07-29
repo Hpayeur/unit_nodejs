87ec6218d4ed79ce20c824edcd9016e379dd825e
Hpayeur <hailpaye017@gmail.com>
2026-07-29 12:55:52 -0600

    Routes part 1

--- nodejs_unit/demo/app.test.js
+++ nodejs_unit/demo/app.test.js
@@ -6,7 +6,68 @@ const sinon = require("sinon");
 const sinonChai = require("sinon-chai").default;
 chai.use(sinonChai);
 const rewire = require("rewire");
+const request = require("supertest");
 
-// var sandbox = sinon.sandbox.create() has changed into sinon.createSandbox().
+var app = rewire("./app");
+var users = require("./users");
+var auth = require("./auth");
 var sandbox = sinon.createSandbox();
-var mailer = rewire("./mailer");
+
+describe("app", () => {
+  afterEach(() => {
+    app = rewire("./app");
+    sandbox.restore();
+  });
+  context("GET /", () => {
+    it("should get /", (done) => {
+      request(app)
+        .get("/")
+        .expect(200)
+        .end((err, response) => {
+          expect(response.body)
+            .to.have.property("name")
+            .to.equal("Foo Fooing Bar");
+          done(err);
+        });
+    });
+  });
+
+  context("POST /user", () => {
+    let createStub, errorStub;
+    it("should call user.create", (done) => {
+      createStub = sandbox.stub(users, "create").resolves({ name: "foo" });
+
+      request(app)
+        .post("/user")
+        .send({ name: "fake" })
+        .expect(200)
+        .end((err, response) => {
+          expect(createStub).to.have.been.calledOnce;
+          expect(response.body).to.have.property("name").to.equal("foo");
+          done(err);
+        });
+    });
+
+    it("should call handleError on error", (done) => {
+      createStub = sandbox
+        .stub(users, "create")
+        .rejects(new Error("fake_error"));
+
+      errorStub = sandbox.stub().callsFake((res, error) => {
+        return res.status(400).json({ error: "fake" });
+      });
+      app.__set__("handleError", errorStub);
+
+      request(app)
+        .post("/user")
+        .send({ name: "fake" })
+        .expect(400)
+        .end((err, response) => {
+          expect(createStub).to.have.been.calledOnce;
+          expect(errorStub).to.have.been.calledOnce;
+          expect(response.body).to.have.been.property("error").to.equal("fake");
+          done(err);
+        });
+    });
+  });
+});
